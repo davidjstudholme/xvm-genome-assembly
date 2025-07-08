e17b71555c557353eb98d370e2580a7717546439
David Studholme <d.j.studholme@exeter.ac.uk>
2025-07-08 13:34:10 +0000

    reconciled contigs

--- 05_trycycler/commands.sh
+++ 05_trycycler/commands.sh
@@ -1,14 +1,26 @@
-### Activateconda environment
+### Activate Conda environment
 #conda activate trycycler_env
 conda list -n phame_env > trycycler_env_packages.txt
 
 
+### Cluster the contigs
 for strain in NCPPB_2005 NCPPB_2251 NCPPB_4379 NCPPB_4381 NCPPB_4395; do
     echo $strain
-    
     trycycler cluster --assemblies ../04_long-read_assemblies/$strain.assemblies/*.fasta --reads ../03_ont_trimmed/$strain.ont.filtlong.fastq.gz  --out_dir $strain.trycycler
-
 done
 
+### Reconcile the contigs
 
- 
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2005.ont.filtlong.fastq.gz --cluster_dir NCPPB_2005.trycycler/cluster_001
+
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2251.ont.filtlong.fastq.gz --cluster_dir NCPPB_2251.trycycler/cluster_001
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2251.ont.filtlong.fastq.gz --cluster_dir NCPPB_2251.trycycler/cluster_002
+
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4379.ont.filtlong.fastq.gz --cluster_dir NCPPB_4379.trycycler/cluster_001
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4379.nt.filtlong.fastq.gz --cluster_dir NCPPB_4379.trycycler/cluster_007
+
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4381.ont.filtlong.fastq.gz --cluster_dir NCPPB_4381.trycycler/cluster_001
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4381.ont.filtlong.fastq.gz --cluster_dir NCPPB_4381.trycycler/cluster_001
+
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4395.ont.filtlong.fastq.gz --cluster_dir NCPPB_4395.trycycler/cluster_001
+trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4395.ont.filtlong.fastq.gz --cluster_dir NCPPB_4395.trycycler/cluster_002
