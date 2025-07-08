b6e2b6ac76141e7a7f1efa138564967e063d960b
David Studholme <d.j.studholme@exeter.ac.uk>
2025-07-08 14:10:04 +0000

    Done MSA

--- 05_trycycler/commands.sh
+++ 05_trycycler/commands.sh
@@ -9,18 +9,44 @@ for strain in NCPPB_2005 NCPPB_2251 NCPPB_4379 NCPPB_4381 NCPPB_4395; do
     trycycler cluster --assemblies ../04_long-read_assemblies/$strain.assemblies/*.fasta --reads ../03_ont_trimmed/$strain.ont.filtlong.fastq.gz  --out_dir $strain.trycycler
 done
 
+### Generate dotplots
+trycycler dotplot --cluster_dir NCPPB_2005.trycycler/cluster_001
+trycycler dotplot --cluster_dir NCPPB_2251.trycycler/cluster_001
+trycycler dotplot --cluster_dir NCPPB_2251.trycycler/cluster_002
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_001
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_002
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_003
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_004
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_005
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_006
+trycycler dotplot --cluster_dir NCPPB_4379.trycycler/cluster_007
+trycycler dotplot --cluster_dir NCPPB_4381.trycycler/cluster_001
+trycycler dotplot --cluster_dir NCPPB_4381.trycycler/cluster_002
+trycycler dotplot --cluster_dir NCPPB_4395.trycycler/cluster_001
+trycycler dotplot --cluster_dir NCPPB_4395.trycycler/cluster_002
+
 ### Reconcile the contigs
-
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2005.ont.filtlong.fastq.gz --cluster_dir NCPPB_2005.trycycler/cluster_001
-
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2251.ont.filtlong.fastq.gz --cluster_dir NCPPB_2251.trycycler/cluster_001
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_2251.ont.filtlong.fastq.gz --cluster_dir NCPPB_2251.trycycler/cluster_002
-
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4379.ont.filtlong.fastq.gz --cluster_dir NCPPB_4379.trycycler/cluster_001
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4379.nt.filtlong.fastq.gz --cluster_dir NCPPB_4379.trycycler/cluster_007
-
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4381.ont.filtlong.fastq.gz --cluster_dir NCPPB_4381.trycycler/cluster_001
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4381.ont.filtlong.fastq.gz --cluster_dir NCPPB_4381.trycycler/cluster_001
-
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4395.ont.filtlong.fastq.gz --cluster_dir NCPPB_4395.trycycler/cluster_001
 trycycler reconcile --reads ../03_ont_trimmed/NCPPB_4395.ont.filtlong.fastq.gz --cluster_dir NCPPB_4395.trycycler/cluster_002
+
+### Multiple sequence alignment
+trycycler  msa --cluster_dir NCPPB_2005.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_2251.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_2251.trycycler/cluster_002
+trycycler  msa --cluster_dir NCPPB_4379.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_4379.trycycler/cluster_007
+trycycler  msa --cluster_dir NCPPB_4381.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_4381.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_4395.trycycler/cluster_001
+trycycler  msa --cluster_dir NCPPB_4395.trycycler/cluster_002
+
+
+
+
